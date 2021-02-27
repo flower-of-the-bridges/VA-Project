3386214fa19afef3bd92c7a7027e2802577e8436
ai18 <antonio.ionta.18@gmail.com>
2021-02-27 12:10:53 +0100

    Significative Dates JSON

---
+++ src/significative_dates.js
@@ -0,0 +1,10 @@
+export default {
+    [
+        {
+            label: "lockdown_start", date: "2020-03-09"
+        },
+        {
+            label: "lockdown_end", date: "2020-05-18"
+        }
+    ]    
+}
