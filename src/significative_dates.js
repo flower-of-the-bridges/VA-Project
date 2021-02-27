11a5bfef094ff1c98cec23b22714c58f2d6f9fa3
FlowerOfTheBridges <gfiordeponti@gmail.com>
2021-02-28 00:34:44 +0100

    fix

--- src/significative_dates.js
+++ src/significative_dates.js
@@ -1,11 +1,14 @@
 export const significativeDates = [
     {
-        label: "Inizio Lockdown Nazionale", date: "2020-03-09"
+        label: "Start: General Lockdown", date: "2020-03-09"
     },
     {
-        label: "Fine Lockdown Nazionale", date: "2020-05-18"
+        label: "Finish: General Lockdown", date: "2020-05-18"
     },
     {
-        label: "Inizio Sistema a Zone", date: "2020-11-04"
+        label: "Start: Curfew", date: "2020-10-18"
+    },
+    {
+        label: "Start: Localized Lockdown", date: "2020-11-04"
     }
 ]    
